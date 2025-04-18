c575db8d470323868abcc0af67270d38a09ef951
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-04-18 03:11:39 -0400

    Update MySQL 2056.sql

--- MySQL 2056.sql
+++ MySQL 2056.sql
@@ -1,3 +1,4 @@
+-- THIS IS A QUESTION BY AMAZON
 # Write a query that will calculate the number of shipments per month. The unique key for one shipment is a combination of shipment_id and sub_id. Output the year_month in format YYYY-MM and the number of shipments in that month.
 SELECT
   COUNT(shipment_id) AS count_shipment_id,
