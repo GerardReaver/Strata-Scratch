dc5232de4a69eccece8460d7d347be7c760ce3a1
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-04-18 02:23:46 -0400

    Update MySQL 9728.sql

--- MySQL 9728.sql
+++ MySQL 9728.sql
@@ -1,4 +1,6 @@
 --CITY OF SAN FRANCISCO ASKED FOR THIS TYPE OF QUESTION FOR SOME DATA ANALYST GOV JOBS. 
+# You are given a dataset of health inspections that includes details about violations. Each row represents an inspection, and if an inspection resulted in a violation, the violation_id column will contain a value.
+# Count the total number of violations that occurred at 'Roxanne Cafe' for each year, based on the inspection date. Output the year and the corresponding number of violations in ascending order of the year.
 SELECT
     COUNT(violation_id) AS n_violations,
     YEAR(inspection_date) 
