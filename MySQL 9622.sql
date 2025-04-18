4e92ee1507d5911594806b7d7dbe5c30317d6bb4
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-04-18 02:22:06 -0400

    Update MySQL 9622.sql

--- MySQL 9622.sql
+++ MySQL 9622.sql
@@ -1,4 +1,5 @@
 -- A PROBLEM TO BE SOLVED BY THE AIR BNB COMPANY
+# Find the average number of bathrooms and bedrooms for each city’s property types. Output the result along with the city name and the property type.
  SELECT
      city,
      property_type,
