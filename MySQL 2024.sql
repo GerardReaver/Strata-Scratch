63244e4d032b2a70f114265cf465c941b88f7523
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-04-18 02:21:00 -0400

    Create MySQL 2024.sql

---
+++ MySQL 2024.sql
@@ -0,0 +1,10 @@
+-- THIS IS A QUESTION BY APPLE, DELL AND MICROSOFT
+# Write a query that returns the number of unique users per client per month.
+SELECT
+    COUNT(DISTINCT user_id),
+    client_id,
+    MONTH(time_id)
+FROM
+    fact_events
+GROUP BY
+    client_id, MONTH(time_id)
