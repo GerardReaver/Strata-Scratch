d490f0f798991f3fd0b922431cb908fc8a57db01
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-04-18 03:10:52 -0400

    Create MySQL 2056.sql

---
+++ MySQL 2056.sql
@@ -0,0 +1,11 @@
+# Write a query that will calculate the number of shipments per month. The unique key for one shipment is a combination of shipment_id and sub_id. Output the year_month in format YYYY-MM and the number of shipments in that month.
+SELECT
+  COUNT(shipment_id) AS count_shipment_id,
+  DATE_FORMAT(shipment_date, '%Y-%m') AS date_ym
+FROM
+  amazon_shipment
+GROUP BY
+  DATE_FORMAT(shipment_date, '%Y-%m')
+ORDER BY
+  date_ym;
+-- THIS LESSON TAUGHT HOW TO FORMAT DATES FOR DIFFERENT TYPES OF ANALYSIS
