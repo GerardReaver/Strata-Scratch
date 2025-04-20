d78550d38c556c4a984efdacc00fe70d60230b38
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-04-19 23:19:05 -0400

    Update MySQL 9653.sql

--- MySQL 9653.sql
+++ MySQL 9653.sql
@@ -4,10 +4,12 @@
 # Sort the result based on the event count in the descending order.
 SELECT
     event_name,
-    COUNT(event_name)
+    COUNT(event_name) AS event_count
 FROM
     playbook_events
 WHERE
     device = 'macbook pro'
 GROUP BY
     event_name
+ORDER BY
+    event_count DESC
