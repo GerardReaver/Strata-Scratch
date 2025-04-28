9629e96981cf83dc5b68de24dd2de4f827e172bd
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-04-28 10:34:59 -0400

    Update MySQL 9847.sql

--- MySQL 9847.sql
+++ MySQL 9847.sql
@@ -2,7 +2,7 @@
 # Find the number of workers by department who joined on or after April 1, 2014. Output the department name along with the corresponding number of workers. Sort the results based on the number of workers in descending order. 
 SELECT
     department,
-    COUNT(worker_id) AS nun_workers
+    COUNT(worker_id) AS num_workers
 FROM
     worker
 WHERE
@@ -10,4 +10,4 @@ WHERE
 GROUP BY
     department
 ORDER BY
-    nun_workers DESC
+    num_workers DESC
